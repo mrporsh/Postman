167201cc17218fe710017c333183d9bbaf46a735
Habib <mrporsh58@gmail.com>
2020-07-20 13:10:14 -0400

    secretkey updated

--- Postman/settings.py
+++ Postman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2m(5#+8za#0hn)-_=f@tr*h22df7$%8!rbp1u4%%_&f-ft&b-_'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
